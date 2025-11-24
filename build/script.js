93425b5b9d7c63279dd660ca8dc626e46366b1cc
Areeb Ahmad <syedareebahmad7100@gmail.com>
2025-11-25 01:14:56 +0530

    Initial Commit

---
+++ build/script.js
@@ -0,0 +1,32 @@
+import { WORDS } from "./words.js";
+
+const MAX_GUESSES = 6;
+let guessesRemaining = MAX_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+
+console.log(rightGuessString);
+
+function initBoard() {
+    let board = document.getElementById("gameBoard")
+
+    for (let i = 0; i < MAX_GUESSES; i++) {
+        
+        let row = document.createElement("div")
+        row.className = "letterRow"
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letterBox"
+
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+
+    }
+}
+
+initBoard()
